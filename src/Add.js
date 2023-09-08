0f4373c07cbbbda3284607830d5238b0d93f20c8
bhavinv89 <51201786+bhavinv89@users.noreply.github.com>
2023-09-07 21:22:02 -0400

    Add.js (Update )

--- src/Add.js
+++ src/Add.js
@@ -1,56 +1,123 @@
-import React from "react";
-import ItemForm from "./ItemForm";
-import { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import {Card, CardBody, CardTitle, CardText, Button, Form, FormGroup, Label, Input} from "reactstrap";
 
-function Add({ setSnacks, setDrinks, currentSnacks, currentDrinks }) {
-    const [data, setData] = useState({
-      name: "",
-      description: "",
-      recipe: "",
-      serve: "",
-    });
-  
-    const handleFormSubmit = () => {
-      // Validate the data here
-      if (data.name) {
-        // Add the new drink to the list of drinks
-        const newDrinks = [...currentDrinks, data];
-        setDrinks(newDrinks);
-        setData({}); // Clear the form fields
-      }
-    };
-  
-    if (data.name) {
-      return (
-        <div>
-          <p>{data.name} added!</p>
-          <p>
-            Note: Please allow infinite time to have the {data.name} added as we
-            currently have no actual database.
-          </p>
-          <p>
-            <b>Recipe:</b> {data.recipe}
-          </p>
-          <p>
-            <b>Serve:</b> {data.serve}
-          </p>
-          <Link to="/">Go Home</Link>
-        </div>
-      );
-    }
-  
-    return (
-      <div>
-        <h2>Add a New Drink</h2>
-        <ItemForm
-          data={data}
-          setData={setData}
-          handleFormSubmit={handleFormSubmit}
-          itemType="drink"
-        />
-      </div>
-    );
+/** Form for adding a menu item.
+ *
+ * Props:
+ * - addItem: call this to add item in parent
+ *
+ * State:
+ * - local state for each field on form
+ *
+ */
+
+function Add({ addItem }) {
+  const [form, setForm] = useState({
+    type: "snacks",
+    name: "",
+    description: "",
+    recipe: "",
+    serve: ""
+  });
+  const history = useHistory();
+
+  const handleChange = evt => {
+    const { name, value } = evt.target;
+    setForm(f => ({
+      ...f,
+      [name]: value
+    }));
   }
-  
-  export default Add;
+
+  const handleSubmit = evt => {
+    evt.preventDefault();
+    let { type, ...data } = form;
+    addItem(type, data);
+    history.push("/");
+  }
+
+  const { type, name, description, recipe, serve } = form;
+
+  return (
+    <section className="col-md-4">
+      <Card>
+        <CardBody>
+          <CardTitle className="font-weight-bold text-center">
+            Add Item
+          </CardTitle>
+          <CardText>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </CardText>
+
+          <Form onSubmit={handleSubmit}>
+            <FormGroup>
+              <Label for="type">Type</Label>
+              <Input
+                type="select"
+                name="type"
+                id="type"
+                value={type}
+                onChange={handleChange}
+              >
+                <option>snacks</option>
+                <option>drinks</option>
+              </Input>
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="name">Name</Label>
+              <Input
+                type="text"
+                name="name"
+                id="name"
+                value={name}
+                onChange={handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="description">Description</Label>
+              <Input
+                type="textarea"
+                name="description"
+                id="description"
+                value={description}
+                onChange={handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="recipe">Recipe</Label>
+              <Input
+                type="textarea"
+                name="recipe"
+                id="recipe"
+                value={recipe}
+                onChange={handleChange}
+              />
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="serve">Serve</Label>
+              <Input
+                type="textarea"
+                name="serve"
+                id="serve"
+                value={serve}
+                onChange={handleChange}
+              />
+            </FormGroup>
+
+            <Button className="float-right btn btn-outline-light">
+              Add Item
+            </Button>
+          </Form>
+        </CardBody>
+      </Card>
+    </section>
+  );
+}
+
+export default Add;
